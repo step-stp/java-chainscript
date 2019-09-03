b2eb3f9b46276ce6c1a7b2ee5ced658ed7f1d6a7
Step <stepstpdev@gmail.com>
2019-09-03 14:48:41 +0300

    Merging Changes -- adding registerTypeHierarchyAdapter

--- src/main/java/com/stratumn/chainscript/utils/JsonHelper.java
+++ src/main/java/com/stratumn/chainscript/utils/JsonHelper.java
@@ -12,7 +12,7 @@
   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
   See the License for the specific language governing permissions and
   limitations under the License.
-*/
+*/ 
 package com.stratumn.chainscript.utils;
 
 import java.io.IOException;
@@ -28,55 +28,58 @@ import com.google.gson.JsonObject;
 import com.stratumn.canonicaljson.CanonicalJson;
 /***
  * 
- * Wrapper for Json Library Registers type adapter for chainscript types
+ * Wrapper for Json Library 
+ * Registers type adapter for chainscript types
  *
  */
-public class JsonHelper {
-   private static GsonBuilder gsonBuilder;
-
+public class JsonHelper
+{
+   private static GsonBuilder gsonBuilder ;
    /***
-    * Properties set on Gson to standardize gson conversion on all projects
-    * 
+    * Properties set on Gson to standardize gson conversion
+    * on all projects
     * @return
     */
-   private static GsonBuilder getGsonBuilder() {
-      if (gsonBuilder == null) {
+   private static GsonBuilder getGsonBuilder()
+   {
+      if(gsonBuilder == null)
+      {
          gsonBuilder = new GsonBuilder()
-               .registerTypeAdapter(stratumn.chainscript.Chainscript.Link.class,
-                     new ProtoGsonAdapter<stratumn.chainscript.Chainscript.Link>(
-                           stratumn.chainscript.Chainscript.Link.class))
-               .registerTypeAdapter(stratumn.chainscript.Chainscript.Segment.class,
-                     new ProtoGsonAdapter<stratumn.chainscript.Chainscript.Segment>(
-                           stratumn.chainscript.Chainscript.Segment.class))
-               .registerTypeAdapter(stratumn.chainscript.Chainscript.Signature.class,
-                     new ProtoGsonAdapter<stratumn.chainscript.Chainscript.Signature>(
-                           stratumn.chainscript.Chainscript.Signature.class))
-               .registerTypeAdapter(stratumn.chainscript.Chainscript.Evidence.class,
-                     new ProtoGsonAdapter<stratumn.chainscript.Chainscript.Evidence>(
-                           stratumn.chainscript.Chainscript.Evidence.class));
+            .registerTypeAdapter(stratumn.chainscript.Chainscript.Link.class,
+               new ProtoGsonAdapter<stratumn.chainscript.Chainscript.Link>(stratumn.chainscript.Chainscript.Link.class))
+            .registerTypeAdapter(stratumn.chainscript.Chainscript.Segment.class,
+               new ProtoGsonAdapter<stratumn.chainscript.Chainscript.Segment>(stratumn.chainscript.Chainscript.Segment.class))
+            .registerTypeAdapter(stratumn.chainscript.Chainscript.Signature.class,
+               new ProtoGsonAdapter<stratumn.chainscript.Chainscript.Signature>(stratumn.chainscript.Chainscript.Signature.class))
+            .registerTypeAdapter(stratumn.chainscript.Chainscript.Evidence.class,
+               new ProtoGsonAdapter<stratumn.chainscript.Chainscript.Evidence>(stratumn.chainscript.Chainscript.Evidence.class))
+            
+            ;
          ;
+ 
          gsonBuilder.serializeNulls().disableHtmlEscaping();
       }
       return gsonBuilder;
    }
-
-   public static Gson getGson() {
-
+   
+   public static Gson getGson()
+   { 
+      
       return getGsonBuilder().create();
    }
-
+   
    /***
     * Expose registeration of new type adapters
-    * 
     * @param type
     * @param typeAdapter
     */
-   public static void registerTypeAdapter(Type type, Object typeAdapter) {
-
+   public static void registerTypeAdapter (Type type, Object typeAdapter)
+   {
+       
       getGsonBuilder().registerTypeAdapter(type, typeAdapter);
-
+      
    }
-
+   
    /***
     * Registers a type adapter hierarchy on the internal gson builder 
     * @param clazz
@@ -91,54 +94,67 @@ public class JsonHelper {
    
    /***
     * converts the json object to an object of the type specified.
-    * 
     * @param json
     * @param classOfT
     * @return
     */
-   public static <T> T fromJson(String json, Class<T> classOfT) {
+   public  static <T>  T fromJson(String json ,  Class<T> classOfT)
+   {
       return getGson().fromJson(json, classOfT);
    }
-
-   public static <T> T fromJson(JsonElement json, Class<T> typeOfT) {
-      return getGson().fromJson(json, typeOfT);
+   
+   /***
+    * converts an jsonelement to the object of type t
+    * @param json
+    * @param typeOfT
+    * @return
+    */
+   public  static <T>  T fromJson(JsonElement json , Class<T> typeOfT)
+   {
+      return  getGson().fromJson(json, typeOfT);
    }
-
+   
+   
+   
+   
    /***
     * produces a canonicalized json of the object
-    * 
     * @param src
     * @return
     * @throws IOException
     */
-   public static String toCanonicalJson(Object src) throws IOException {
-      String json = getGson().toJson(src);
+   public static String toCanonicalJson( Object src) throws IOException
+   {
+      String json =  getGson().toJson(src);
       return CanonicalJson.canonizalize(json);
    }
 
    /***
-    * Converts an object or JsonString to a map
-    * 
+    * Converts an object or JsonString to a map 
     * @param srcObject
     * @return tree map of all object properties
-    */
+    */  
    @SuppressWarnings("unchecked")
-   public static Map<String, Object> objectToMap(Object srcObject) {
-      return (Map<String, Object>) objectToObject(srcObject, Map.class);
+   public static Map<String,Object> objectToMap(Object srcObject) 
+   {   
+       return (Map<String,Object>)objectToObject(srcObject, Map.class ) ;
    }
+   
+ 
+    
    /***
-    * Converts a map to an object of type T
-    * 
+    * Converts a map to an object of type T 
     * @param srcMap
-    * @param tClass
+    * @param tClass 
     * @return an object deserialized from the properties on the map.
     */
-   public static <T> T mapToObject(Map<String, Object> srcMap, Class<T> tClass) {
-      String json = getGson().toJson(srcMap);
-      T tObject = getGson().fromJson(json, tClass);
-      return tObject;
+   public static <T> T mapToObject(Map<String,Object> srcMap, Class<T> tClass) 
+   {
+       String json = getGson().toJson(srcMap);
+       T tObject =  getGson().fromJson(json,  tClass);
+       return tObject;
    }
-
+   
    /***
     * Convenience method to map object to another 
     * Excample map to object or vice versa
@@ -166,48 +182,49 @@ public class JsonHelper {
    
    /***
     * Produces normal Json String
-    * 
     * @param src
     * @return
+    * @throws IOException
     */
-   public static String toJson(Object src) {
-      String json = getGson().toJson(src);
+   public static String toJson( Object src)  
+   { 
+      String json =  getGson().toJson(src);
       return json;
    }
-
-   private static final Pattern indexPattern = Pattern.compile("\\[(\\d+)\\]");
-
+   
+   
+   private static final Pattern indexPattern = Pattern.compile("\\[(\\d+)\\]"); 
    /***
-    * recursively extracts data from JsonElement by path A.B.C or A.B.C[i] or
-    * A.B[i].C
-    * 
+    * recursively extracts data from JsonElement by path A.B.C or A.B.C[i] or A.B[i].C
     * @param data
     * @param path
     * @return
     */
-   public static JsonElement getData(JsonElement data, String path) {
-      int firstSep = path.indexOf('.');
-      String nextKey = firstSep == -1 ? path : path.substring(0, firstSep);
-      int arrIndex = -1;
+   public static JsonElement getData(JsonElement data,String path )
+   { 
+      int firstSep = path.indexOf('.'); 
+      String nextKey =firstSep==-1? path :  path.substring(0,firstSep);
+      int arrIndex = -1; 
       data = data.getAsJsonObject().get(nextKey);
-
-      if (data.isJsonNull())
-         return null;
-      // extract index from array.
+      
+      if (data.isJsonNull()) return null; 
+      //extract index from array. 
       Matcher indexMatcher = indexPattern.matcher(nextKey);
-      if (indexMatcher.find()) {
+      if (indexMatcher.find())
+      {
          arrIndex = Integer.parseInt(indexMatcher.group(1));
-         if (arrIndex >= 0 && data.isJsonArray())
+         if (arrIndex >=0 && data.isJsonArray() )
             data = data.getAsJsonArray().get(arrIndex);
       }
-
-      // if not last key in path recurse
-      if (firstSep != -1) {
-         path = path.substring(firstSep + 1);
-         data = getData(data, path);
+      
+      //if not last key  in path recurse
+      if (firstSep!=-1)
+      { 
+         path = path.substring(firstSep+1);
+         data = getData(data,path);
       }
-
+      
       return data;
    }
-
+   
 }
